bb6bcd5cac32bcf420a121f1ade961eb0030b7c6
GCSEguide <106030039+GCSEguide@users.noreply.github.com>
2024-03-27 20:12:22 +0000

    update

---
+++ src/statistics.rs
@@ -0,0 +1,11 @@
+#[derive(Default)]
+pub struct Stats{
+    pub fps: Vec<[f64;2]>,
+    step: usize,
+}
+impl Stats{
+    pub fn update(&mut self,framerate: f64){
+        self.fps.push([self.step as f64,framerate]);
+        self.step+=1;
+    }
+}
